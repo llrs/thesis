57a65c4e6e4bfc05fd6893a48a472e7112dc1d98
llrs <lluis.revilla@gmail.com>
2020-10-16 12:42:22 +0200

    Formatting and style update

--- _build.sh
+++ _build.sh
@@ -2,3 +2,4 @@
 
 Rscript -e "bookdown::render_book('index.Rmd', output_format = 'bookdown::gitbook', encoding = 'UTF-8')"
 Rscript -e "bookdown::render_book('index.Rmd', output_format = 'bookdown::pdf_book', encoding = 'UTF-8')"
+Rscript -e "bookdown::render_book('index.Rmd', output_format = 'bookdown::epub_book', encoding = 'UTF-8')"
