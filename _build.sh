c24308cdfdbde8b5ff7ea778d0cfac7e40a45dc6
llrs <lluis.revilla@gmail.com>
2021-09-09 09:31:10 +0200

    Fix bash

--- _build.sh
+++ _build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 Rscript -e "bookdown::render_book('index.Rmd', output_format = 'bookdown::gitbook', encoding = 'UTF-8')"
 Rscript -e "bookdown::render_book('index.Rmd', output_format = 'bookdown::pdf_book', encoding = 'UTF-8')"
