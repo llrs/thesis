30abe4eb2138b081d5df97f5288cff991ad23fb7
llrs <lluis.revilla@gmail.com>
2021-12-23 17:26:39 +0100

    Reorder preference of output

--- _build.sh
+++ _build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 Rscript -e "bookdown::render_book('index.Rmd', output_format = 'bookdown::gitbook', encoding = 'UTF-8')"
-Rscript -e "bookdown::render_book('index.Rmd', output_format = 'bookdown::pdf_book', encoding = 'UTF-8')"
 Rscript -e "bookdown::render_book('index.Rmd', output_format = 'bookdown::epub_book', encoding = 'UTF-8')"
+Rscript -e "bookdown::render_book('index.Rmd', output_format = 'bookdown::pdf_book', encoding = 'UTF-8')"
