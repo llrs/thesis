878e1a4907605d48ef5376ba60c4b73358dc3d6d
llrs <lluis.revilla@gmail.com>
2022-01-28 17:51:44 +0100

    Remove build as it is done in different steps

--- _build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-Rscript -e "bookdown::render_book('index.Rmd', output_format = 'bookdown::gitbook', encoding = 'UTF-8')"
-Rscript -e "bookdown::render_book('index.Rmd', output_format = 'bookdown::epub_book', encoding = 'UTF-8', output_dir = '.'')"
-Rscript -e "bookdown::render_book('index.Rmd', output_format = 'bookdown::pdf_book', encoding = 'UTF-8', output_dir = '.')"
